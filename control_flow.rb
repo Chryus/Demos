4d30e1ef9632b1cb7b7ee594fded0024469cb7f9
Chryus <chris.haack@me.com>
2013-10-21 16:40:45 -0400

    control flow demo

---
+++ control_flow.rb
@@ -0,0 +1,25 @@
+=begin
+
+a = 4
+
+if a == 1
+	puts "a is 1"
+elsif  a == 2
+	puts "a is 2"
+else 
+	puts "I don't know the value of a"
+end
+
+=end
+#it's easier to use a case statment then then if elseif...
+
+a = 4
+
+case a
+when 1
+	puts "a is 1"
+when 2
+	puts "a is 2"
+else 
+	puts "I don't know the value of a"
+end
