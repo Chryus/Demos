19a8cd6f0922a2e572f5f1622eee3a22d9a9b921
Chryus <chris.haack@me.com>
2013-10-22 10:39:08 -0400

    for loops demo

---
+++ loops-4.rb
@@ -0,0 +1,19 @@
+for num in 1..15 =
+  puts num
+end
+
+for num in 1…15
+  puts num
+end
+
+for num in 1..20
+    puts num
+end
+
+for num in 1...100
+	puts num
+end
+
+for num in 1..100
+	puts
+end
